7f3d23dcde36fa8ca34717e2979176582fa41c7d
ruthvikvishwa <ruthvikvishwa@gmail.com>
2026-05-20 20:37:02 +0000

    Fix CI lint warnings for Vercel build

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,36 +4,36 @@ import { AiFillGithub } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
 
 function Footer() {
-  return (
-    <Container fluid className="footer">
-      <Row>
-        <Col md="4" className="footer-body">
-          <ul className="footer-icons">
-            <li className="social-icons">
-              <a
-                href="https://github.com/ruthvikvishwa"
-                style={{ color: "white" }}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <AiFillGithub />
-              </a>
-            </li>
-            <li className="social-icons">
-              <a
-                href="https://www.linkedin.com/in/ruthvik-vishwa/"
-                style={{ color: "white" }}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <FaLinkedinIn />
-              </a>
-            </li>
-          </ul>
-        </Col>
-      </Row>
-    </Container>
-  );
+return (
+<Container fluid className="footer">
+<Row>
+<Col md="4" className="footer-body">
+<ul className="footer-icons">
+<li className="social-icons">
+<a
+href="https://github.com/ruthvikvishwa"
+style={{ color: "white" }}
+target="_blank"
+rel="noopener noreferrer"
+>
+<AiFillGithub />
+</a>
+</li>
+<li className="social-icons">
+<a
+href="https://www.linkedin.com/in/ruthvik-vishwa/"
+style={{ color: "white" }}
+target="_blank"
+rel="noopener noreferrer"
+>
+<FaLinkedinIn />
+</a>
+</li>
+</ul>
+</Col>
+</Row>
+</Container>
+);
 }
 
 export default Footer;
