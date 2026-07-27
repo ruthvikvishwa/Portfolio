dc078b50f32fb45740b05bf9cc63d40e5c303b23
ruthvikvishwa <ruthvikvishwa@gmail.com>
2026-07-27 14:40:08 -0500

    Correct name and job title in AboutCard component

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -1,28 +1,27 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
 import { ImPointRight } from "react-icons/im";
-
 function AboutCard() {
   return (
     <Card className="quote-card-view">
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi everyone! I’m <span className="purple">Ruthvik Vishwanthwar</span>{" "}
+            Hi everyone! I'm <span className="purple">Ruthvik Vishwanathwar</span>{" "}
             from <span className="purple">Frisco, Texas, USA</span>.
             <br />
-            I’m currently working as a{" "}
-            <span className="purple">Data Engineer</span> at{" "}
-            <span className="purple">Sentara Health</span>.
-            <br />I hold a Masters degree in{" "}
+            I currently work as a{" "}
+            <span className="purple">Senior Data Engineer</span> at{" "}
+            <span className="purple">Sentara Health</span>, where I focus on
+            building and modernizing data pipelines on Azure Databricks.
+            <br />I hold a Master's degree in{" "}
             <span className="purple">Computer Science</span> from{" "}
-            <span className="purple">New York Institute Of Technology, Manhattan.</span>.
+            <span className="purple">New York Institute of Technology, Manhattan</span>.
             <br />
             <br />
             Outside of coding, I love engaging in activities that keep me
             creative and inspired:
           </p>
-
           <ul>
             <li className="about-activity">
               <ImPointRight /> Playing Billiards 🎱🥢
@@ -31,7 +30,6 @@ function AboutCard() {
               <ImPointRight /> Traveling and Exploring New Places 🌍
             </li>
           </ul>
-
           <p style={{ color: "rgb(155 126 172)" }}>
             "Strive to build things that make a difference!"{" "}
           </p>
@@ -40,5 +38,4 @@ function AboutCard() {
     </Card>
   );
 }
-
 export default AboutCard;
